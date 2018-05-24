cab891c8ccc0a1c3a1ad1910e948a0eba30a2c0b
Lin Lu <lulin.velvet@qq.com>
2018-05-24 16:45:56 +0200

    Lab5 done

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,10 @@
 create table reservation(
       id int not null primary key,
-      courseId varchar_ignorecase(50) not null,
+      courseid varchar_ignorecase(50) not null,
       status varchar_ignorecase(50) not null
 );
 CREATE TABLE statistic(
       id int NOT NULL PRIMARY KEY ,
-      methodName varchar_ignorecase(50) NOT NULL ,
-      openTime int NOT NULL
+      methodname varchar_ignorecase(50) NOT NULL ,
+      opentime int NOT NULL
 );
